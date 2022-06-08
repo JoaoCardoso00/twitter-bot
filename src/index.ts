7ca1a6802782ead1a25a99661632039afe541277
JoaoCardoso00 <jvcq13@gmail.com>
2022-06-08 20:50:26 -0300

    initial bot setup

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-console.log("hellodwa ");
-console.log("a");
+import { rwClient } from "./twitterClient";
+import axios from "axios";
+import sunTzu from "sun-tzu-quotes";
+
+const tweet = async () => {
+  try {
+
+    await rwClient.v2.tweet(`"${sunTzu()}" - Sun Tzu, The Art of War`);
+
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// setInterval(yourFunction, 1000 * 60 * 60);
+
+tweet();
---
+++ src/twitterClient.ts
@@ -0,0 +1,11 @@
+import { TwitterApi } from "twitter-api-v2";
+import "dotenv/config";
+
+const client = new TwitterApi({
+  appKey: process.env.APP_KEY,
+  appSecret: process.env.APP_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessSecret: process.env.TOKEN_SECRET,
+});
+
+export const rwClient = client.readWrite;
